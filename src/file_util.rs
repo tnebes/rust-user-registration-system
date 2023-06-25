6efb7cd00f4789d750e8a40f93f8e2e2a7874703
tnebes <tomislav.nebes@gmail.com>
2023-06-25 19:26:53 +0200

    Refactored the code to be better

--- src/file_util.rs
+++ src/file_util.rs
@@ -11,84 +11,71 @@ const FILE_NAME: &str = "data.json";
 const USER_DIRECTORY: &str = "user_data";
 
 pub fn file_exists() -> bool {
-    let file_path_name = get_file_path_name();
-    return file_or_directory_exists(&file_path_name);
+    return file_or_directory_exists(&get_file_path_name());
 }
 
 fn get_file_path_name() -> String {
-    let current_dir = env::current_dir().unwrap();
-    let current_dir_string = current_dir.to_str().unwrap();
-    let mut file_path_name = String::from(current_dir_string);
-    file_path_name.push_str(MAIN_SEPARATOR.to_string().as_str());
-    file_path_name.push_str(USER_DIRECTORY);
-    file_path_name.push_str(MAIN_SEPARATOR.to_string().as_str());
-    file_path_name.push_str(FILE_NAME);
-    return file_path_name;
+    let mut file_path = std::path::PathBuf::new();
+    file_path.push(env::current_dir().unwrap());
+    file_path.push(USER_DIRECTORY);
+    file_path.push(FILE_NAME);
+    file_path.to_string_lossy().to_string()
 }
-
 pub fn create_file() -> String {
     let file_path_name = get_file_path_name();
-    let file_path = std::path::Path::new(file_path_name.as_str());
-    let file_path_directory = file_path.parent().unwrap().to_str().unwrap().to_string();
-    if !file_or_directory_exists(&file_path_directory) {
-        info!("Creating directory {}", file_path_directory);
-        std::fs::create_dir(&file_path_directory).unwrap();
+    let file_path = std::path::Path::new(&file_path_name);
+
+    if let Some(parent_directory) = file_path.parent() {
+        if let Err(error) = std::fs::create_dir_all(parent_directory) {
+            error!("Failed to create directory: {:?}", error);
+            return String::new();
+        }
     }
-    let file = std::fs::File::create(file_path);
-    if file.is_err() {
+
+    if let Err(error) = std::fs::File::create(&file_path) {
+        error!("Failed to create file: {:?}", error);
         return String::new();
     }
-    return file_path_name;
+
+    file_path_name
 }
 
 fn file_or_directory_exists(file_path_name: &String) -> bool {
-    let file_path = std::path::Path::new(file_path_name.as_str());
-    return file_path.exists();
+    std::path::Path::new(file_path_name).exists()
 }
 
 pub fn write_to_file(user: &User) -> bool {
     let mut users = get_users_from_json();
     users.push(user.clone());
     let serialised_users = serde_json::to_string(&users).unwrap();
-    let mut file = match OpenOptions::new().write(true).truncate(true).open(&get_file_path_name()) {
-        Ok(file) => file,
-        Err(error) => {
-            error!("Could not open file for writing {:?}", error);
-            return false;
-        }
-    };
-    return match file.write_all(&serialised_users.as_bytes()) {
-        Ok(()) => true,
-        Err(error) => {
-            error!("Could not write users to json {:?}", error);
-            false
-        }
-    };
+
+    if let Err(error) = std::fs::write(&get_file_path_name(), &serialised_users) {
+        error!("Could not write users to JSON: {:?}", error);
+        return false;
+    }
+    true
 }
 
 pub fn get_users_from_json() -> Vec<User> {
     let json_content = read_user_file();
-    let users: Vec<User> = serde_json::from_str(&json_content)
+    serde_json::from_str(&json_content)
         .unwrap_or_else(|error| {
             error!("Failed to parse json content: {:?}", error);
             Vec::new()
-        });
-    users
+        })
 }
 
 pub fn read_user_file() -> String {
     let file_path = get_file_path_name();
-    let mut file = match std::fs::File::open(&file_path) {
-        Ok(file) => file,
-        Err(error) => {
-            error!("Could not read file at path {:?} {}", file_path, error);
-            return String::new();
+
+    match std::fs::read_to_string(&file_path) {
+        Ok(content) => {
+            info!("Successfully read file at {}, size is {}", file_path, content.len());
+            content
         }
-    };
-    let mut buffer = String::new();
-    match file.read_to_string(&mut buffer) {
-        Ok(_file) => info!("Successfully read file at {}, size is {}", file_path, _file),
-        Err(error) => error!("Failed to read file at {} {}", file_path, error)
+        Err(error) => {
+            error!("Failed to read file at {} {}", file_path, error);
+            String::new()
+        }
     }
-    return buffer.to_string();
 }
--- src/main.rs
+++ src/main.rs
@@ -32,11 +32,9 @@ fn main() {
 }
 
 fn initialise_logger() {
-    for arg in std::env::args() {
-        if arg == "log" {
-            print_util::print_logging_header();
-            std::env::set_var("RUST_LOG", "info");
-        }
+    if std::env::args().any(|arg| arg == "log") {
+        print_util::print_logging_header();
+        std::env::set_var("RUST_LOG", "info");
     }
     env_logger::init();
 }
@@ -45,24 +43,27 @@ fn register_user(user: &User) -> bool {
     if !file_util::file_exists() {
         let file_name = file_util::create_file();
         if file_name.is_empty() {
-            error!("Failed to create file at {}", &file_name);
+            error!("Failed to create file at {}", file_name);
             println!("Please check the integrity of the user data file and try again.");
             return false;
         }
-        info!("Created file: {}", &file_name)
+        info!("Created file: {}", file_name);
     }
-    if user_exists(&user) {
-        info!("User {} {}, {} already exists", &user.first_name, &user.last_name, &user.age);
+
+    if user_exists(user) {
+        info!("{:?} already exists", user);
         println!("You are already registered!");
         return false;
     }
-    if !file_util::write_to_file(&user) {
+
+    if !file_util::write_to_file(user) {
         error!("Failed to write to file");
         println!("Please check the integrity of the user data file and try again.");
         return false;
     }
-    println!("Thank you, {}, you are now registered!", &user.first_name);
-    return true;
+
+    println!("Thank you, {}, you are now registered!", user.first_name);
+    true
 }
 
 fn user_exists(user: &User) -> bool {
@@ -75,48 +76,52 @@ fn is_user_age_valid(user: &User) -> bool {
 }
 
 fn get_user_from_input() -> User {
-    let mut user = User {
-        first_name: String::new(),
-        last_name: String::new(),
-        age: 0,
-    };
-    let first_name: String = get_input("Enter your first name:");
-    if first_name.is_empty() {
-        error!("First name {} cannot be blank", &first_name);
-        println!("First name cannot be blank.");
-        return get_user_from_input();
-    }
-    let last_name: String = get_input("Enter your last name:");
-    if last_name.is_empty() {
-        error!("Last name {} cannot be blank", &last_name);
-        println!("Last name cannot be blank.");
-        return get_user_from_input();
-    }
-    let age_string: String = get_input("Enter your age:");
-    if age_string.is_empty() {
-        error!("Age {} cannot be blank", &age_string);
-        println!("Age cannot be blank.");
-        return get_user_from_input();
-    }
-    let age: u32 = match age_string.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            error!("Age {} must be a number", &age_string);
-            println!("Age must be a number.");
-            return get_user_from_input();
+    loop {
+        let mut user = User {
+            first_name: String::new(),
+            last_name: String::new(),
+            age: 0,
+        };
+
+        user.first_name = get_input("Enter your first name:");
+        if user.first_name.is_empty() {
+            error!("First name cannot be blank");
+            println!("First name cannot be blank.");
+            continue;
         }
-    };
-    user.first_name = first_name;
-    user.last_name = last_name;
-    user.age = age;
-    info!("{:?} created", user);
-    return user;
+
+        user.last_name = get_input("Enter your last name:");
+        if user.last_name.is_empty() {
+            error!("Last name cannot be blank");
+            println!("Last name cannot be blank.");
+            continue;
+        }
+
+        let age_string = get_input("Enter your age:");
+        if age_string.is_empty() {
+            error!("Age cannot be blank");
+            println!("Age cannot be blank.");
+            continue;
+        }
+
+        if let Ok(age) = age_string.trim().parse() {
+            user.age = age;
+            info!("{:?} created", user);
+            return user;
+        } else {
+            error!("Age must be a number");
+            println!("Age must be a number.");
+            continue;
+        }
+    }
 }
 
 fn get_input(prompt_text: &str) -> String {
-    let mut input = String::new();
     println!("{}", prompt_text);
     io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
-    return input.trim().to_string();
+
+    input.trim().to_string()
 }
